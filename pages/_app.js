fe8a61a07754df8b0f1c5a0406a791890a902476
Chen Zhung <46520696+Xectroz@users.noreply.github.com>
2022-01-13 21:01:35 +0800

    change _app.js structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,34 @@
 import '../styles/globals.css';
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+const wsLink = process.browser ? new WebSocketLink({ // if you instantiate in the server, the error will be thrown
+  uri: `ws://localhost:4000/subscriptions`,
+  options: {
+    reconnect: true
+  }
+}) : null;
+
+const httplink = new HttpLink({
+	uri: 'http://localhost:4000/graphql',
+	credentials: 'same-origin'
+});
+
+const link = process.browser ? split( //only create the split in the browser
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httplink,
+) : httplink;
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: link,
   cache: new InMemoryCache(),
 });
 
