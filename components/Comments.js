779a20767cb5ecc56be6c2c4c37c4d35bdb1653f
Chen Zhung <46520696+Xectroz@users.noreply.github.com>
2022-01-20 12:04:19 +0800

    taskdetails

--- components/Comments.js
+++ components/Comments.js
@@ -1,10 +1,19 @@
 import styles from '../styles/TaskDetails.module.css';
 
-function Comments() {
+function Comments({data}) {
+  let shortName = '';
+
+  if (data.user.username.split(' ').length > 1) {
+  let name1 = data.user.username.split(' ').shift()[0].toUpperCase();
+  let name2 = data.user.username.split(' ').pop()[0].toUpperCase();
+  shortName = name1 + name2;
+  } else {
+  shortName = data.user.username[0].toUpperCase();
+  }
   return (
-    <div className="d-flex">
-      <div className={styles.circle}>LI</div>
-      <p className="px-3 pt-2">ni de comment blabla</p>
+    <div className={"d-flex " + styles.pad }>
+      <div className={styles.circle}>{shortName}</div>
+      <p className="px-3 pt-2">{data.comment}</p>
     </div>
   );
 }
--- pages/workspace/taskdetails/[id].js
+++ pages/workspace/taskdetails/[id].js
@@ -6,11 +6,13 @@ import {useRouter} from 'next/router';
 import Comments from '../../../components/Comments';
 import {gql, useQuery, useMutation} from '@apollo/client';
 import {useState, useEffect} from 'react'
+import Swal from 'sweetalert2'
 
 function TaskDetails() {
   const Router = useRouter();
   const [task, setTask] = useState(null)
   const {id} = Router.query;
+
   const GET_TASK_DETAILS = gql`
     query GetIndTask($getIndTaskId: String) {
       getIndTask(id: $getIndTaskId) {
@@ -33,6 +35,21 @@ function TaskDetails() {
         workspace {
           owner
           id
+          name
+        }
+        user{
+          id 
+          username
+        }
+      }
+    }
+  `
+
+  const ADD_COMMENT = gql`
+    mutation AddTaskComment($id : ID, $user : String, $comment : String) {
+      addTaskComment(task : {id : $id, user : $user, comment : $comment}) {
+        comments {
+          comment
         }
       }
     }
@@ -44,12 +61,34 @@ function TaskDetails() {
     }
   })
 
+  const [addComment, addCommentData] = useMutation(ADD_COMMENT)
+  
+
   useEffect(() => {
     if(getTaskData.data){
       setTask(getTaskData.data.getIndTask)
     }
   }, [getTaskData])
 
+  const commentHandler = () => {
+      let comment = document.getElementById('comment').value
+      if(comment.length > 0 && comment.length < 50){
+        addComment({
+          variables : {
+            id,
+            user : JSON.parse(localStorage.getItem('userData')).user.id,
+            comment
+          }
+        })
+      }
+  }
+
+  useEffect(() => {
+    if(addCommentData.data){
+      getTaskData.refetch()
+      document.getElementById('comment').value = ''
+    }
+  }, [addCommentData.data])
 
   return (
     <>
@@ -60,7 +99,7 @@ function TaskDetails() {
         <Row>
           <Col lg="2">
             <Nav defaultActiveKey="/workspace" className={styles.sideNav}>
-              <Navbar.Brand className={styles.navBrand}>adad</Navbar.Brand>
+              <Navbar.Brand className={styles.navBrand}>{task.workspace.name}</Navbar.Brand>
               <div className={styles.navLink} onClick={() => Router.push(`/workspace/taskboard/${id}`)}>
                 <i className={'fas fa-tasks ' + styles.icon}></i>
                 Task Board
@@ -71,27 +110,28 @@ function TaskDetails() {
             <Container>
               <div className="py-3">
                 <h2>{task.title}</h2>
-                <h4>{task.description}</h4>
-                <p className="text-end">Created by: CHENZHUNGGG</p>
-                <p className="pt-5">{"Assign to : " + task.assigns.}</p>
+                <br />
+                <h4>Description : {task.description}</h4>
+                <br />
+                <p>{"Date Created : " + Date(task.date).toString()}</p>
+                <p className="text-end">{"Created by : " + task.user.username}</p>
+                <p className="pt-5">{"Assign to : " + task.assigns.map(e => e.username).join(', ')}</p>
               </div>
               <div>
                 <Card>
                   <Card.Body>
                     <Card.Title>Comments</Card.Title>
-  
-                    {/* Map */}
-                    <Comments />
+                    {task.comments.map(comment => <Comments data={comment} />) }
                   </Card.Body>
                   <Card.Footer className="py-3">
                     <Row>
                       <Col lg="10">
                         <Form.Group>
-                          <Form.Control type="text" placeholder="Write a comment" />
+                          <Form.Control type="text" placeholder="Write a comment" id="comment" />
                         </Form.Group>
                       </Col>
                       <Col lg="2">
-                        <Button>Enter</Button>
+                        <Button onClick={commentHandler}>Enter</Button>
                       </Col>
                     </Row>
                   </Card.Footer>
