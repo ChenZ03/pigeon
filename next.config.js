13003340874a142fabd46a304745db7ab021f2e5
Chen Zhung <46520696+Xectroz@users.noreply.github.com>
2022-01-24 10:34:09 +0800

    add yarnn

--- next.config.js
+++ next.config.js
@@ -17,9 +17,5 @@ module.exports = {
   // resolve: {
   //   extensions: ['.ts', '.js'],
   // },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  
 }
