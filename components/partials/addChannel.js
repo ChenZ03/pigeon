2ef379169d8bd23e56d1581ef9be3e99097a2446
Jackhe <jackhefoong@gmail.com>
2022-01-24 11:14:35 +0800

    enter to submit

--- components/partials/addChannel.js
+++ components/partials/addChannel.js
@@ -1,67 +1,64 @@
 import styles from '../../styles/WorkspaceChat.module.css';
 import {gql, useMutation} from '@apollo/client';
 import {useEffect} from 'react';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
-function AddChannel({channelList, setAddChannel, id , refetch}){
-
-    const AddChannel = gql`
-        mutation AddChannel($channel_name : String, $workspace_id : String) {
-            addChannel(workspace: {
-            channel_name : $channel_name,
-            workspace_id : $workspace_id
-            }) {
-                channels
-            }
-        }
-    `
-
-    const [add, addData] = useMutation(AddChannel)
-
-    const channelNameList = channelList.map(chn => chn.name.toLowerCase())
-
-    const acceptHandler = () => {
-        let channelName = document.getElementById('channelName').value.toLowerCase()
-        console.log(channelName)
-        if(channelName.length < 1){
-            Swal.fire(
-                "Error",
-                "Name must be at least 1 character",
-                "error"
-            )
-            return
-        }
-        if(channelNameList.includes(channelName)){
-            Swal.fire(
-                "Error",
-                "Channel Exists in this workspace",
-                "error"
-            )
-            return
-        }
-        add({
-            variables : {
-                channel_name : channelName,
-                workspace_id : id
-            }
-        })
-
+function AddChannel({channelList, setAddChannel, id, refetch}) {
+  const AddChannel = gql`
+    mutation AddChannel($channel_name: String, $workspace_id: String) {
+      addChannel(workspace: {channel_name: $channel_name, workspace_id: $workspace_id}) {
+        channels
+      }
     }
+  `;
 
-    useEffect(() => {
-        if(addData.data){
-            setAddChannel(false)
-            refetch()
-        }
-    }, [addData])
-    return(
-        <div className={styles.navLink3}>
-            <input type="text" placeholder="Channel Name" className={styles.channelInput} id="channelName"/>
-            <button type="submit" className={styles.accept} onClick={acceptHandler}>
-              ✔
-            </button>
-        </div>
-    )
+  const [add, addData] = useMutation(AddChannel);
+
+  const channelNameList = channelList.map((chn) => chn.name.toLowerCase());
+
+  const acceptHandler = () => {
+    let channelName = document.getElementById('channelName').value.toLowerCase();
+    console.log(channelName);
+    if (channelName?.length < 1) {
+      Swal.fire('Error', 'Name must be at least 1 character', 'error');
+      return;
+    }
+    if (channelNameList.includes(channelName)) {
+      Swal.fire('Error', 'Channel Exists in this workspace', 'error');
+      return;
+    }
+    add({
+      variables: {
+        channel_name: channelName,
+        workspace_id: id,
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (addData.data) {
+      setAddChannel(false);
+      refetch();
+    }
+  }, [addData]);
+  return (
+    <div className={styles.navLink3}>
+      <input
+        type="text"
+        placeholder="Channel Name"
+        className={styles.channelInput}
+        id="channelName"
+        onKeyPress={(e) => {
+          if (e.key == 'Enter') {
+            acceptHandler();
+          }
+        }}
+      />
+      <button type="submit" className={styles.accept} onClick={acceptHandler}>
+        ✔
+      </button>
+    </div>
+  );
 }
 
-export default AddChannel
+export default AddChannel;
